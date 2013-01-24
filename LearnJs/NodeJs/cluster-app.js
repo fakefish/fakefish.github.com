cd26be69e6f35e45653c2fb66a0fe668512c21c2
王佳裕 <fakefish.wjy@gmail.com>
2013-01-24 10:08:43 +0800

    remove LearnJs

--- LearnJs/NodeJs/cluster-app.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var cluster = require('cluster');
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello,World!\n');
-});
-cluster(server).set('workers',2).use(cluster.reload());
--- LearnJs/NodeJs/connect/index.js
+++
@@ -1,8 +0,0 @@
-var connect = require('connect');
-var app = connect()
-  .use(connect.logger('dev'))
-  .use(connect.static('public'))
-  .use(function(req, res){
-  	res.end('hello world\n');
-  })
-  .listen(3000);
